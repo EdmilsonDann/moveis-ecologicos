ac0f4b52fbc41a64cb4e800dd4d6ced7090add9b
EdmilsonDann <edmilsondaniell@gmail.com>
2018-11-19 07:52:00 -0200

    Diminuindo intervalo de traoca de banner

--- principal.js
+++ principal.js
@@ -1,4 +1,4 @@
-var banners =
+﻿var banners =
 ["Os melhores do Brasil!", "Qualidade e preço baixo!"];
 var bannerAtual = 0;
 function trocaBanner() {
@@ -6,4 +6,4 @@ bannerAtual = (bannerAtual + 1) % 2;
 document.querySelector(’h2#mensagem’).textContent =
 banners[bannerAtual];
 }
-setInterval(trocaBanner, 2000);
+setInterval(trocaBanner, 1000);
